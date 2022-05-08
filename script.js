fbe45a3a685df450bc968b136bf383e611fcaa68
Abhishek ✪ <72429449+abyseku@users.noreply.github.com>
2022-05-08 13:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleNavbar() {
+    var navbar = document.getElementById('myNavbar');
+    navbar.classList.toggle('responsive');
+}
